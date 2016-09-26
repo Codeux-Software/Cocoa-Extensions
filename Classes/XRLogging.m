Codeux-Software/Cocoa-Extensions

commit 9a2561d10fae45f6b19740a0457e0e2b3a9b848b

Modify logging behavior
• Pass the subsystem to the string formatting function
• If unified logging is being used, then do two things:
1. Do not filter out debug messages at this point. Allow those to be
passed to os_log and have that filter out debug messages.
2. Do not specify the type in the formatted string. Console and other
utilities already show what the type is.
• _LogToConsoleFormatMessage_v1() still exists to prevent crashes from
apps that already built against the macro.
_LogToConsoleFormatMessage_v2() takes its place as the function that is
able to take the subsystem as an argument.

Classes/XRLogging.m
@@ -54,20 +54,20 @@ LogToConsoleSubsystemType _CSFrameworkInternalLogSubsystem(void)
 
 #if _LogToConsoleSupportsUnifiedLogging == 1
 static LogToConsoleSubsystemType _LogToConsoleDefaultSubsystemValue = NULL;
+
+static BOOL _LogToConsoleIsUsingUnifiedLogging = NO;
 #endif
 
 LogToConsoleSubsystemType _LogToConsoleDefaultSubsystem(void)
 {
 #if _LogToConsoleSupportsUnifiedLogging == 1
-	static BOOL isUsingOSXSierraOrLater = NO;
-
 	static dispatch_once_t onceToken;
 
 	dispatch_once(&onceToken, ^{
-		isUsingOSXSierraOrLater = [XRSystemInformation isUsingOSXSierraOrLater];
+		_LogToConsoleIsUsingUnifiedLogging = [XRSystemInformation isUsingOSXSierraOrLater];
 	});
 
-	if (isUsingOSXSierraOrLater == NO) {
+	if (_LogToConsoleIsUsingUnifiedLogging == NO) {
 		return NULL;
 	}
 
@@ -95,12 +95,29 @@ void _LogToConsoleSetDebugLoggingEnabled(BOOL enabled)
 	_LogToConsoleDebugLoggingEnabled = enabled;
 }
 
-NSString * _Nullable _LogToConsoleFormatMessage_v1_arg(u_int8_t type, const char *filename, const char *function, unsigned long line, const char *formatter, va_list arguments)
+NSString * _Nullable _LogToConsoleFormatMessage_v1_arg(LogToConsoleSubsystemType subsystem, u_int8_t type, const char *filename, const char *function, unsigned long line, const char *formatter, va_list arguments)
 {
 	NSCParameterAssert(formatter != NULL);
 	NSCParameterAssert(filename != NULL);
 	NSCParameterAssert(function != NULL);
 
+	if (type == LogToConsoleTypeDebug) {
+		BOOL logDebug =
+
+#if _LogToConsoleSupportsUnifiedLogging == 1
+		(_LogToConsoleDebugLoggingEnabled || _LogToConsoleIsUsingUnifiedLogging);
+#else
+		_LogToConsoleDebugLoggingEnabled;
+#endif
+
+		if (logDebug == NO) {
+			return nil;
+		}
+	}
+
+	NSString *formatString = nil;
+
+	if (_LogToConsoleIsUsingUnifiedLogging == NO) {
 	const char *typeString = NULL;
 
 	switch (type) {
@@ -112,10 +129,6 @@ NSString * _Nullable _LogToConsoleFormatMessage_v1_arg(u_int8_t type, const char
 		}
 		case LogToConsoleTypeDebug:
 		{
-			if (_LogToConsoleDebugLoggingEnabled == NO) {
-				return nil;
-			}
-
 			typeString = "Debug";
 
 			break;
@@ -139,10 +152,10 @@ NSString * _Nullable _LogToConsoleFormatMessage_v1_arg(u_int8_t type, const char
 		}
 	}
 
-	/* It would be faster to use a version of vsprintf() here but the reason
-	 that I don't is because that requires managing buffer size which NSString
-	 takes care of for us when formatting. */
-	NSString *formatString = [NSString stringWithFormat:@"[%s] %s [Line %d]: %s", typeString, function, line, formatter];
+		formatString = [NSString stringWithFormat:@"[%s] %s [Line %d]: %s", typeString, function, line, formatter];
+	} else {
+		formatString = [NSString stringWithFormat:@"%s [Line %d]: %s", function, line, formatter];
+	}
 
 	formatString = [formatString stringByReplacingOccurrencesOfString:@"%{public}" withString:@"%"];
 
@@ -150,6 +163,25 @@ NSString * _Nullable _LogToConsoleFormatMessage_v1_arg(u_int8_t type, const char
 
 	return formattedString;
 }
+
+NSString *_LogToConsoleFormatMessage_v2(LogToConsoleSubsystemType subsystem, u_int8_t type, const char *filename, const char *function, unsigned long line, const char *formatter, ...)
+{
+	NSCParameterAssert(formatter != NULL);
+	NSCParameterAssert(filename != NULL);
+	NSCParameterAssert(function != NULL);
+
+	va_list arguments;
+	va_start(arguments, formatter);
+
+	NSString *formattedString = _LogToConsoleFormatMessage_v1_arg(subsystem, type, filename, function, line, formatter, arguments);
+
+	va_end(arguments);
+
+	return formattedString;
+}
+
+#pragma mark -
+#pragma mark Deprecated
 
 NSString *_LogToConsoleFormatMessage_v1(u_int8_t type, const char *filename, const char *function, unsigned long line, const char *formatter, ...)
 {
@@ -160,7 +192,7 @@ NSString *_LogToConsoleFormatMessage_v1(u_int8_t type, const char *filename, con
 	va_list arguments;
 	va_start(arguments, formatter);
 
-	NSString *formattedString = _LogToConsoleFormatMessage_v1_arg(type, filename, function, line, formatter, arguments);
+	NSString *formattedString = _LogToConsoleFormatMessage_v1_arg(LogToConsoleDefaultSubsystem(), type, filename, function, line, formatter, arguments);
 
 	va_end(arguments);
 
@@ -184,7 +216,7 @@ void _LogToConsoleNSLogShim(const char *formatter, const char *filename, const c
 	va_list arguments;
 	va_start(arguments, line);
 
-	NSString *formattedString = _LogToConsoleFormatMessage_v1_arg(LogToConsoleTypeDefault, filename, function, line, formatter, arguments);
+	NSString *formattedString = _LogToConsoleFormatMessage_v1_arg(LogToConsoleDefaultSubsystem(), LogToConsoleTypeDefault, filename, function, line, formatter, arguments);
 
 	va_end(arguments);
 
@@ -208,7 +240,7 @@ void _LogToConsoleNSLogShim_v2(u_int8_t type, const char *filename, const char *
 	va_list arguments;
 	va_start(arguments, formatter);
 
-	NSString *formattedString = _LogToConsoleFormatMessage_v1_arg(type, filename, function, line, formatter, arguments);
+	NSString *formattedString = _LogToConsoleFormatMessage_v1_arg(LogToConsoleDefaultSubsystem(), type, filename, function, line, formatter, arguments);
 
 	va_end(arguments);
 

Classes/XRLogging.h
@@ -46,7 +46,7 @@ NS_ASSUME_NONNULL_BEGIN
 		if (_subsystemPtr == NULL) {	\
 			_subsystemPtr = LogToConsoleDefaultSubsystem();		\
 		}	\
-		NSString *_formattedMessage = _LogToConsoleFormatMessage(_type, _format, ##__VA_ARGS__);	\
+		NSString *_formattedMessage = _LogToConsoleFormatMessage(_subsystemPtr, _type, _format, ##__VA_ARGS__);	\
 		if (_formattedMessage != nil) {		\
 			_LogToConsoleFormattedMessage(_subsystemPtr, _type, _formattedMessage);	\
 		}	\
@@ -81,8 +81,8 @@ NS_ASSUME_NONNULL_BEGIN
 	_LogToConsoleWithSubsystemAndType(_subsystem, LogToConsoleTypeInfo, _format, ##__VA_ARGS__)
 
 /* _LogToConsoleFormattedMessage() */
-#define _LogToConsoleFormatMessage(_type, _format, ...)		\
-	_LogToConsoleFormatMessage_v1(_type, __FILE__, __PRETTY_FUNCTION__, __LINE__, _format, ##__VA_ARGS__)
+#define _LogToConsoleFormatMessage(_subsystem, _type, _format, ...)		\
+	_LogToConsoleFormatMessage_v2(_subsystem, _type, __FILE__, __PRETTY_FUNCTION__, __LINE__, _format, ##__VA_ARGS__)
 
 /* LogToConsoleSetDebugLoggingEnabled() */
 #define LogToConsoleSetDebugLoggingEnabled(_enabled)	\
@@ -110,7 +110,7 @@ NS_ASSUME_NONNULL_BEGIN
 	_LogToConsoleWithSubsystemAndType(_subsystem, _type, "Current Stack: %@", [NSThread callStackSymbols]);
 
 /* Helper functions (private) */
-COCOA_EXTENSIONS_EXTERN NSString * _Nullable _LogToConsoleFormatMessage_v1(u_int8_t type, const char *filename, const char *function, unsigned long line, const char *formatter, ...);
+COCOA_EXTENSIONS_EXTERN NSString * _Nullable _LogToConsoleFormatMessage_v2(LogToConsoleSubsystemType subsystem, u_int8_t type, const char *filename, const char *function, unsigned long line, const char *formatter, ...);
 
 void _LogToConsoleSetDebugLoggingEnabled(BOOL enabled);
 

commit 34f800d067eccaa4ade7014f0561fa8ece56646f

Only check _LogToConsoleIsUsingUnifiedLogging if _LogToConsoleSupportsUnifiedLogging == 1
Fix a small bug introduced in 9a2561d that prevents compilation when
_LogToConsoleSupportsUnifiedLogging is not enabled.

Classes/XRLogging.m
@@ -117,7 +117,11 @@ NSString * _Nullable _LogToConsoleFormatMessage_v1_arg(LogToConsoleSubsystemType
 
 	NSString *formatString = nil;
 
+#if _LogToConsoleSupportsUnifiedLogging == 1
 	if (_LogToConsoleIsUsingUnifiedLogging == NO) {
+#else
+	if (1) {
+#endif
 	const char *typeString = NULL;
 
 	switch (type) {