Codeux-Software/Cocoa-Extensions

commit e53723d9a225f72f0da2d051bacc2a02fe60a1c8

Refactor logging
Logging guts have been completely replaced with a Swift implementation which the C functions are bridged to.

Notes:
• New implementation no longer strips out "{public}" formatters. While these are still no longer supported, it's up to the caller to make sure supported formatters are used.
• There is no longer a separate implementation when compiling on an SDK that doesn't have unified logging. That has been around long enough that it is now considered standard.
• LogCurrentStackTrace() functions have been renamed to LogStackTrace(). Available functions include: LogStackTrace(), LogStackTraceWithSubystem(), LogStackTraceWithType(), and LogStackTraceWithArguments(),
• Old implementation functions will return a deprecation warning and simple formatted string. Can't remove them entirely because some stuff may still link against them.

Classes/XRLogging.swift
@@ -1,0 +1,226 @@
+/* *********************************************************************
+*
+*            Copyright (c) 2018 Codeux Software, LLC
+*     Please see ACKNOWLEDGEMENT for additional information.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions
+* are met:
+*
+*  * Redistributions of source code must retain the above copyright
+*    notice, this list of conditions and the following disclaimer.
+*  * Redistributions in binary form must reproduce the above copyright
+*    notice, this list of conditions and the following disclaimer in the
+*    documentation and/or other materials provided with the distribution.
+*  * Neither the name of "Codeux Software, LLC", nor the names of its
+*    contributors may be used to endorse or promote products derived
+*    from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+* ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+* OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+* HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+* LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+* OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+* SUCH DAMAGE.
+*
+*********************************************************************** */
+
+import os.log
+
+///
+/// Logging acts as a facility for logging messages, regardless of macOS version.
+/// Because it's designed to be backwards compatible, it isn't as technical savvy
+/// as something such as os_log which allows much more powerful formatters.
+/// At worst, assume NSLog() is used; so design the log message around that.
+///
+@objc(XRLogging)
+public class Logging : NSObject
+{
+	///
+	/// Subsystems are favored in this order:
+	///
+	/// 1. "subsystem" argument of function
+	/// 2. defaultSubsystem value (default = nil)
+	/// 3. OSLog.default (on macOS 10.12 and later)
+	///
+	@objc
+	public static var defaultSubsystem: OSLog?
+
+	///
+	/// Flag to enable debug log type
+	///
+	/// macOS 10.12 and later control debug logging
+	/// at the system level which negates this flag.
+	///
+	/// See: os_log_debug_enabled()
+	///
+	@objc
+	public static var debugLogging = false
+
+	@objc
+	public enum Types : Int {
+		case `default` = 0
+
+		case debug = 1
+		case info = 2
+		case error = 3
+		case fault = 4
+
+		@available(OSX 10.12, *)
+		fileprivate var systemType:OSLogType
+		{
+			switch self {
+				case .debug:
+					return OSLogType.debug
+				case .info:
+					return OSLogType.info
+				case .error:
+					return OSLogType.error
+				case .fault:
+					return OSLogType.fault
+				default:
+					return OSLogType.default
+			}
+		} // systemType
+
+		public var description: String {
+			switch self {
+				case .debug:
+					return "Debug"
+				case .info:
+					return "Info"
+				case .error:
+					return "Error"
+				case .fault:
+					return "Fault"
+				default:
+					return "Default"
+			}
+		} // description
+
+		fileprivate var formatterDescription: String?
+		{
+			/* No need to show description when performing
+			 unified logging because system captrues this. */
+			if #available(OSX 10.12, *) {
+				return nil
+			}
+
+			return "[\(description)] "
+		} // formatterDescription
+	}
+
+	fileprivate struct Context
+	{
+		var subsystem: OSLog?
+		var type: Types = .`default`
+		var file: String
+		var line: Int
+		var column: Int
+		var function: String
+	}
+
+	public static func log(_ message: String, _ arguments: CVarArg..., type: Types, subsystem: OSLog?, file: String, line: Int, column: Int, function: String)
+	{
+		log(message, arguments: getVaList(arguments), type: type, subsystem: subsystem, file: file, line: line, column: column, function: function)
+	}
+
+	public static func log(_ message: String, arguments: CVaListPointer, type: Types, subsystem: OSLog?, file: String, line: Int, column: Int, function: String)
+	{
+		let context = Context(subsystem: subsystem, type: type, file: file, line: line, column: column, function: function)
+
+		let formattedMessage = NSString(format: message, arguments: arguments) as String
+
+		log(formattedMessage, in: context)
+	}
+
+	@objc(logMessage:asType:inSubsystem:file:line:column:function:)
+	public static func log(_ formattedMessage: String, type: Types, subsystem: OSLog?, file: String, line: Int, column: Int, function: String)
+	{
+		let context = Context(subsystem: subsystem, type: type, file: file, line: line, column: column, function: function)
+
+		log(formattedMessage, in: context)
+	}
+
+	@objc(logStackTraceSymbols:asType:inSubsystem:)
+	public static func logStackTrace(trace: [String], type: Types, subsystem: OSLog?)
+	{
+		/* Details such as file, line, column, and function are useless
+		 when logging symbols but so I don't have to spend a day modifying
+		 the facility to accept those as optional, just give it defaults. */
+		log("Current Stack: %@", trace, type: type, subsystem: subsystem, file: #file, line: #line, column: #column, function: #function)
+	}
+
+	fileprivate static func log(_ formattedMessage: String, in context: Context)
+	{
+		let messageToLog = "\(context.type.formatterDescription ?? "")\(context.function) [\(context.line):\(context.column)]: \(formattedMessage)"
+
+		guard #available(OSX 10.12, *) else {
+			if (context.type == .debug) {
+				if (debugLogging == false) {
+					return
+				}
+			}
+
+			NSLog(formattedMessage)
+
+			return
+		}
+
+		let subsystem = (context.subsystem ?? defaultSubsystem ?? OSLog.default)
+
+		/* os_log wants a StaticString (compile-time string) which we cannot offer.
+		 We therefore have to use a formatter to format the already formatted string. */
+		os_log("%{public}@", dso: #dsohandle, log: subsystem, type: context.type.systemType, messageToLog)
+	}
+
+	/* Subsystem used by the Cocoa Extensions framework for logging */
+	@objc
+	internal var frameworkSubsystem: OSLog? = {
+		if #available(OSX 10.12, *) {
+			return OSLog(subsystem: "com.codeux.frameworks.CocoaExtensions", category: "General")
+		}
+
+		return nil
+	}()
+}
+
+// MARK - Convenience Functions
+
+/// We can't use macros in Swift which is why all the extra arguments exist.
+/// Unless there is a very, very, very specific to do so, don't set anything
+/// after subsystem as you will just be lying to your own logs.
+public func LogToConsole(_ message: String, _ arguments: CVarArg..., type: Logging.Types = .`default`, subsystem: OSLog? = nil, file: String = #file, line: Int = #line, column: Int = #column, function: String = #function)
+{
+	Logging.log(message, arguments: getVaList(arguments), type: type, subsystem: subsystem, file: file, line: line, column: column, function: function)
+}
+
+public func LogToConsoleDebug(_ message: String, _ arguments: CVarArg..., subsystem: OSLog? = nil, file: String = #file, line: Int = #line, column: Int = #column, function: String = #function)
+{
+	Logging.log(message, arguments: getVaList(arguments), type: .debug, subsystem: subsystem, file: file, line: line, column: column, function: function)
+}
+
+public func LogToConsoleInfo(_ message: String, _ arguments: CVarArg..., subsystem: OSLog? = nil, file: String = #file, line: Int = #line, column: Int = #column, function: String = #function)
+{
+	Logging.log(message, arguments: getVaList(arguments), type: .info, subsystem: subsystem, file: file, line: line, column: column, function: function)
+}
+
+public func LogToConsoleError(_ message: String, _ arguments: CVarArg..., subsystem: OSLog? = nil, file: String = #file, line: Int = #line, column: Int = #column, function: String = #function)
+{
+	Logging.log(message, arguments: getVaList(arguments), type: .error, subsystem: subsystem, file: file, line: line, column: column, function: function)
+}
+
+public func LogToConsoleFault(_ message: String, _ arguments: CVarArg..., subsystem: OSLog? = nil, file: String = #file, line: Int = #line, column: Int = #column, function: String = #function)
+{
+	Logging.log(message, arguments: getVaList(arguments), type: .fault, subsystem: subsystem, file: file, line: line, column: column, function: function)
+}
+
+public func LogStackTrace(type: Logging.Types = .`default`, subsystem: OSLog? = nil, trace: [String] = Thread.callStackSymbols)
+{
+	Logging.logStackTrace(trace: trace, type: type, subsystem: subsystem)
+}

Classes/NSFileManagerHelper.m
@@ -142,7 +142,7 @@ NS_ASSUME_NONNULL_BEGIN
 			LogToConsoleErrorWithSubsystem(_CSFrameworkInternalLogSubsystem(),
 				"isUbiquitous lookup failed: '%@': %@",
 				path, isUbiquitousError.localizedDescription);
-			LogCurrentStackTraceWithSubsystem(_CSFrameworkInternalLogSubsystem());
+			LogStackTraceWithSubsystem(_CSFrameworkInternalLogSubsystem());
 
 			return NO;
 		} else {
@@ -174,7 +174,7 @@ NS_ASSUME_NONNULL_BEGIN
 			LogToConsoleErrorWithSubsystem(_CSFrameworkInternalLogSubsystem(),
 				"isDirectory lookup failed: '%@': %@", path,
 			     isDirectoryError.localizedDescription);
-			LogCurrentStackTraceWithSubsystem(_CSFrameworkInternalLogSubsystem());
+			LogStackTraceWithSubsystem(_CSFrameworkInternalLogSubsystem());
 		}
 
 		return NO;
@@ -189,7 +189,7 @@ NS_ASSUME_NONNULL_BEGIN
 			LogToConsoleErrorWithSubsystem(_CSFrameworkInternalLogSubsystem(),
 				"directoryContents returned nil: '%@': %@",
 				 path, directoryContentsError.localizedDescription);
-			LogCurrentStackTraceWithSubsystem(_CSFrameworkInternalLogSubsystem());
+			LogStackTraceWithSubsystem(_CSFrameworkInternalLogSubsystem());
 
 			return NO;
 		}
@@ -221,7 +221,7 @@ NS_ASSUME_NONNULL_BEGIN
 		LogToConsoleErrorWithSubsystem(_CSFrameworkInternalLogSubsystem(),
 			"isDownloaded lookup failed: '%@': %@",
 			path, isDownloadedError.localizedDescription);
-		LogCurrentStackTraceWithSubsystem(_CSFrameworkInternalLogSubsystem());
+		LogStackTraceWithSubsystem(_CSFrameworkInternalLogSubsystem());
 	}
 
 	return isDownloaded;
@@ -306,7 +306,7 @@ NS_ASSUME_NONNULL_BEGIN
 			LogToConsoleErrorWithSubsystem(_CSFrameworkInternalLogSubsystem(),
 				"Failed to remove file at destination: '%@': %@",
 				[destinationURL path], removeFileError.localizedDescription);
-			LogCurrentStackTraceWithSubsystem(_CSFrameworkInternalLogSubsystem());
+			LogStackTraceWithSubsystem(_CSFrameworkInternalLogSubsystem());
 
 			return NO;
 		}
@@ -327,7 +327,7 @@ NS_ASSUME_NONNULL_BEGIN
 		LogToConsoleErrorWithSubsystem(_CSFrameworkInternalLogSubsystem(),
 			"Failed to copy file to destination: '%@' -> '%@': %@",
 			sourceURL.path, destinationURL.path, copyFileError.localizedDescription);
-		LogCurrentStackTraceWithSubsystem(_CSFrameworkInternalLogSubsystem());
+		LogStackTraceWithSubsystem(_CSFrameworkInternalLogSubsystem());
 
 		return NO;
 	}

Classes/XRLogging.h
@@ -1,88 +1,56 @@
 
-#if defined(AVAILABLE_MAC_OS_X_VERSION_10_12_AND_LATER)
 @import os.log;
-#endif
 
 NS_ASSUME_NONNULL_BEGIN
 
-#if defined(AVAILABLE_MAC_OS_X_VERSION_10_12_AND_LATER)
-#define _LogToConsoleSupportsUnifiedLogging	1
-#else 
-#define _LogToConsoleSupportsUnifiedLogging	0
-#endif
+typedef NS_ENUM(NSInteger, XRLoggingType) {
+	XRLoggingTypeDefault = 0,
+	XRLoggingTypeDebug = 1,
+	XRLoggingTypeInfo = 2,
+	XRLoggingTypeError = 3,
+	XRLoggingTypeFault = 4,
+};
 
-#if _LogToConsoleSupportsUnifiedLogging == 1
-#define LogToConsoleTypeDefault OS_LOG_TYPE_DEFAULT
-#define LogToConsoleTypeInfo OS_LOG_TYPE_INFO
-#define LogToConsoleTypeDebug OS_LOG_TYPE_DEBUG
-#define LogToConsoleTypeError OS_LOG_TYPE_ERROR
-#define LogToConsoleTypeFault OS_LOG_TYPE_FAULT
-
-#define LogToConsoleSubsystemType os_log_t
-
-#define _LogToConsoleFormattedMessage(_subsystem, _type, _formattedMessage)		\
-	if (_subsystem != NULL) {	\
-		os_log_with_type(_subsystem, _type, "%{public}@", _formattedMessage);	\
-} else {	\
-		NSLog(_formattedMessage);		\
-	}
-#else // _LogToConsoleSupportsUnifiedLogging
-#define LogToConsoleTypeDefault 0x00
-#define LogToConsoleTypeInfo 0x01
-#define LogToConsoleTypeDebug 0x02
-#define LogToConsoleTypeError 0x03
-#define LogToConsoleTypeFault 0x04
-
-#define LogToConsoleSubsystemType void *
-
-#define _LogToConsoleFormattedMessage(_subsystem, _type, _formattedMessage)		\
-	NSLog(_formattedMessage);
-#endif
-
-/* _LogToConsoleWithSubsystemAndType() */
-#define _LogToConsoleWithSubsystemAndType(_subsystem, _type, _format, ...)		\
-	do {	\
-		LogToConsoleSubsystemType _subsystemPtr = _subsystem;	\
-		if (_subsystemPtr == NULL) {	\
-			_subsystemPtr = LogToConsoleDefaultSubsystem();		\
-		}	\
-		NSString *_formattedMessage = _LogToConsoleFormatMessage(_subsystemPtr, _type, _format, ##__VA_ARGS__);	\
-		if (_formattedMessage != nil) {		\
-			_LogToConsoleFormattedMessage(_subsystemPtr, _type, _formattedMessage);	\
-		}	\
-	} while (0);
+#define LogToConsoleTypeDefault 	XRLoggingTypeDefault
+#define LogToConsoleTypeInfo 		XRLoggingTypeDebug
+#define LogToConsoleTypeDebug 		XRLoggingTypeInfo
+#define LogToConsoleTypeError 		XRLoggingTypeError
+#define LogToConsoleTypeFault 		XRLoggingTypeFault
 
 /* LogToConsole() */
-#define LogToConsole(_format, ...)	\
-	LogToConsoleWithSubsystem(NULL, _format, ##__VA_ARGS__)
+#define LogToConsole(_message, ...)	\
+	LogToConsoleWithSubsystem(NULL, _message, ##__VA_ARGS__)
 
-#define LogToConsoleWithSubsystem(_subsystem, _format, ...)	\
-	_LogToConsoleWithSubsystemAndType(_subsystem, LogToConsoleTypeDefault, _format, ##__VA_ARGS__)	\
+#define LogToConsoleWithSubsystem(_subsystem, _message, ...)	\
+	_LogToConsole(LogToConsoleTypeDefault, _subsystem, _message, ##__VA_ARGS__)	\
 
 /* LogToConsoleDebug() */
-#define LogToConsoleDebug(_format, ...)	\
-	LogToConsoleDebugWithSubsystem(NULL, _format, ##__VA_ARGS__)
+#define LogToConsoleDebug(_message, ...)	\
+	LogToConsoleDebugWithSubsystem(NULL, _message, ##__VA_ARGS__)
 
-#define LogToConsoleDebugWithSubsystem(_subsystem, _format, ...)	\
-	_LogToConsoleWithSubsystemAndType(_subsystem, LogToConsoleTypeDebug, _format, ##__VA_ARGS__)	\
-
-/* LogToConsoleError() */
-#define LogToConsoleError(_format, ...)	\
-	LogToConsoleErrorWithSubsystem(NULL, _format, ##__VA_ARGS__)
-
-#define LogToConsoleErrorWithSubsystem(_subsystem, _format, ...)	\
-	_LogToConsoleWithSubsystemAndType(_subsystem, LogToConsoleTypeError, _format, ##__VA_ARGS__)	\
+#define LogToConsoleDebugWithSubsystem(_subsystem, _message, ...)	\
+	_LogToConsole(LogToConsoleTypeDebug, _subsystem, _message, ##__VA_ARGS__)	\
 
 /* LogToConsoleInfo() */
-#define LogToConsoleInfo(_format, ...)	\
-	LogToConsoleInfoWithSubsystem(NULL, _format, ##__VA_ARGS__)
+#define LogToConsoleInfo(_message, ...)	\
+	LogToConsoleInfoWithSubsystem(NULL, _message, ##__VA_ARGS__)
 
-#define LogToConsoleInfoWithSubsystem(_subsystem, _format, ...)	\
-	_LogToConsoleWithSubsystemAndType(_subsystem, LogToConsoleTypeInfo, _format, ##__VA_ARGS__)
+#define LogToConsoleInfoWithSubsystem(_subsystem, _message, ...)	\
+	_LogToConsole(LogToConsoleTypeInfo, _subsystem, _message, ##__VA_ARGS__)
 
-/* _LogToConsoleFormattedMessage() */
-#define _LogToConsoleFormatMessage(_subsystem, _type, _format, ...)		\
-	_LogToConsoleFormatMessage_v2(_subsystem, _type, __FILE__, __PRETTY_FUNCTION__, __LINE__, _format, ##__VA_ARGS__)
+/* LogToConsoleError() */
+#define LogToConsoleError(_message, ...)	\
+	LogToConsoleErrorWithSubsystem(NULL, _message, ##__VA_ARGS__)
+
+#define LogToConsoleErrorWithSubsystem(_subsystem, _message, ...)	\
+	_LogToConsole(LogToConsoleTypeError, _subsystem, _message, ##__VA_ARGS__)	\
+
+/* LogToConsoleFault() */
+#define LogToConsoleFault(_message, ...)	\
+	LogToConsoleFaultWithSubsystem(NULL, _message, ##__VA_ARGS__)
+
+#define LogToConsoleFaultWithSubsystem(_subsystem, _message, ...)	\
+	_LogToConsole(LogToConsoleTypeFault, _subsystem, _message, ##__VA_ARGS__)	\
 
 /* LogToConsoleSetDebugLoggingEnabled() */
 #define LogToConsoleSetDebugLoggingEnabled(_enabled)	\
@@ -97,24 +65,31 @@ NS_ASSUME_NONNULL_BEGIN
 	_LogToConsoleSetDefaultSubsystem(_subsystem)
 
 /* LogToConsoleCurrentStackTrace() */
-#define LogToConsoleCurrentStackTrace	\
-	LogCurrentStackTraceWithSubsystem(NULL);
+#define LogStackTrace() 	\
+	LogStackTraceWithSubsystem(NULL);
 
-#define LogCurrentStackTraceWithSubsystem(_subsystem)	\
-	_LogCurrentStackTraceWithSubsystemAndType(_subsystem, LogToConsoleTypeError)
+#define LogStackTraceWithSubsystem(_subsystem) 	\
+	_LogStackTrace(LogToConsoleTypeDefault, _subsystem)
 
-#define LogCurrentStackTraceWithType(_type)	\
-	_LogCurrentStackTraceWithSubsystemAndType(NULL, _type)
+#define LogStackTraceWithType(_type) 	\
+	_LogStackTrace(_type, NULL)
 
-#define _LogCurrentStackTraceWithSubsystemAndType(_subsystem, _type)	\
-	_LogToConsoleWithSubsystemAndType(_subsystem, _type, "Current Stack: %@", [NSThread callStackSymbols]);
+#define LogStackTraceWithArguments(_type, _subsystem) 	\
+	_LogStackTrace(_type, _subsystem)
 
 /* Helper functions (private) */
-COCOA_EXTENSIONS_EXTERN NSString * _Nullable _LogToConsoleFormatMessage_v2(LogToConsoleSubsystemType subsystem, u_int8_t type, const char *filename, const char *function, unsigned long line, const char *formatter, ...);
+#define _LogToConsole(_type, _subsystem, _message, ...)		\
+	_LogToConsoleBridged_v1(_type, _subsystem, __FILE__, __LINE__, 0, __PRETTY_FUNCTION__, _message, ##__VA_ARGS__)
+
+#define _LogStackTrace(_type, _subsystem) 	\
+	_LogStackTraceBridged_v1(_type, _subsystem, [NSThread callStackSymbols])
+
+void _LogToConsoleBridged_v1(XRLoggingType type, os_log_t _Nullable subsystem, const char *file, unsigned long line, unsigned long column, const char *function, const char *message, ...);
+void _LogStackTraceBridged_v1(XRLoggingType type, os_log_t _Nullable subsystem, NSArray<NSString *> *trace);
+
+os_log_t _Nullable _LogToConsoleDefaultSubsystem(void);
+void _LogToConsoleSetDefaultSubsystem(os_log_t _Nullable subsystem);
 
 void _LogToConsoleSetDebugLoggingEnabled(BOOL enabled);
-
-LogToConsoleSubsystemType _Nullable _LogToConsoleDefaultSubsystem(void);
-void _LogToConsoleSetDefaultSubsystem(LogToConsoleSubsystemType _Nullable subsystem);
 
 NS_ASSUME_NONNULL_END

Classes/XRLogging.m
@@ -32,162 +32,73 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-LogToConsoleSubsystemType _Nullable _CSFrameworkInternalLogSubsystem(void)
+os_log_t _Nullable _CSFrameworkInternalLogSubsystem(void)
 {
-	static LogToConsoleSubsystemType _subsystem = NULL;
-
-#if _LogToConsoleSupportsUnifiedLogging == 1
-	static dispatch_once_t onceToken;
-
-	dispatch_once(&onceToken, ^{
-		if (COCOA_EXTENSIONS_RUNNING_ON(10.12, Sierra)) {
-			_subsystem =
-			os_log_create("com.codeux.frameworks.CocoaExtensions", "General");
-		}
-	});
-#endif
-
-	return _subsystem;
+	return [XRLogging frameworkSubsystem];
 }
 
-#if _LogToConsoleSupportsUnifiedLogging == 1
-static LogToConsoleSubsystemType _LogToConsoleDefaultSubsystemValue = NULL;
-
-static BOOL _LogToConsoleIsUsingUnifiedLogging = NO;
-#endif
-
-LogToConsoleSubsystemType _Nullable _LogToConsoleDefaultSubsystem(void)
+os_log_t _Nullable _LogToConsoleDefaultSubsystem(void)
 {
-#if _LogToConsoleSupportsUnifiedLogging == 1
-	static dispatch_once_t onceToken;
-
-	dispatch_once(&onceToken, ^{
-		if (COCOA_EXTENSIONS_RUNNING_ON(10.12, Sierra)) {
-			_LogToConsoleIsUsingUnifiedLogging = YES;
-		}
-	});
-
-	if (_LogToConsoleIsUsingUnifiedLogging == NO) {
-		return NULL;
-	}
-
-	if (_LogToConsoleDefaultSubsystemValue == NULL) {
-		return OS_LOG_DEFAULT;
-	}
-
-	return _LogToConsoleDefaultSubsystemValue;
-#else 
-	return NULL;
-#endif
+	return [XRLogging defaultSubsystem];
 }
 
-void _LogToConsoleSetDefaultSubsystem(LogToConsoleSubsystemType _Nullable subsystem)
+void _LogToConsoleSetDefaultSubsystem(os_log_t _Nullable subsystem)
 {
-#if _LogToConsoleSupportsUnifiedLogging == 1
-	_LogToConsoleDefaultSubsystemValue = subsystem;
-#endif
+	[XRLogging setDefaultSubsystem:subsystem];
 }
-
-static BOOL _LogToConsoleDebugLoggingEnabled = NO;
 
 void _LogToConsoleSetDebugLoggingEnabled(BOOL enabled)
 {
-	_LogToConsoleDebugLoggingEnabled = enabled;
+	[XRLogging setDebugLogging:enabled];
 }
 
-NSString * _Nullable _LogToConsoleFormatMessage_v1_arg(LogToConsoleSubsystemType subsystem, u_int8_t type, const char *filename, const char *function, unsigned long line, const char *formatter, va_list arguments)
+void _LogToConsoleBridged_v1(XRLoggingType type, os_log_t _Nullable subsystem, const char *file, unsigned long line, unsigned long column, const char *function, const char *message, ...)
+{
+	NSCParameterAssert(file != NULL);
+	NSCParameterAssert(function != NULL);
+	NSCParameterAssert(message != NULL);
+
+	va_list arguments;
+	va_start(arguments, message);
+
+	NSString *formattedMessage = [[NSString alloc] initWithFormat:@(message) arguments:arguments];
+
+	va_end(arguments);
+
+	[XRLogging logMessage:formattedMessage asType:type inSubsystem:subsystem file:@(file) line:line column:column function:@(function)];
+}
+
+void _LogStackTraceBridged_v1(XRLoggingType type, os_log_t _Nullable subsystem, NSArray<NSString *> *trace)
+{
+	NSCParameterAssert(trace != nil);
+
+	[XRLogging logStackTraceSymbols:trace asType:type inSubsystem:subsystem];
+}
+
+#pragma mark -
+#pragma mark Deprecated
+
+NSString *_LogToConsoleFormatMessage_v2(os_log_t _Nullable subsystem, u_int8_t type, const char *filename, const char *function, unsigned long line, const char *formatter, ...)
 {
 	NSCParameterAssert(formatter != NULL);
 	NSCParameterAssert(filename != NULL);
 	NSCParameterAssert(function != NULL);
 
-	if (type == LogToConsoleTypeDebug) {
-		BOOL logDebug =
+	static dispatch_once_t onceToken;
 
-#if _LogToConsoleSupportsUnifiedLogging == 1
-		(_LogToConsoleDebugLoggingEnabled || _LogToConsoleIsUsingUnifiedLogging);
-#else
-		_LogToConsoleDebugLoggingEnabled;
-#endif
-
-		if (logDebug == NO) {
-			return nil;
-		}
-	}
-
-	NSString *formatString = nil;
-
-#if _LogToConsoleSupportsUnifiedLogging == 1
-	if (_LogToConsoleIsUsingUnifiedLogging == NO) {
-#endif
-
-		const char *typeString = NULL;
-
-		switch (type) {
-			case LogToConsoleTypeInfo:
-			{
-				typeString = "Info";
-
-				break;
-			}
-			case LogToConsoleTypeDebug:
-			{
-				typeString = "Debug";
-
-				break;
-			}
-			case LogToConsoleTypeError:
-			{
-				typeString = "Error";
-
-				break;
-			}
-			case LogToConsoleTypeFault: {
-				typeString = "Fault";
-
-				break;
-			}
-			default:
-			{
-				typeString = "Default";
-
-				break;
-			}
-		}
-
-		formatString = [NSString stringWithFormat:@"[%s] %s [Line %lu]: %s", typeString, function, line, formatter];
-
-#if _LogToConsoleSupportsUnifiedLogging == 1
-	} else {
-		formatString = [NSString stringWithFormat:@"%s [Line %lu]: %s", function, line, formatter];
-	}
-#endif
-
-	formatString = [formatString stringByReplacingOccurrencesOfString:@"%{public}" withString:@"%"];
-
-	NSString *formattedString = [[NSString alloc] initWithFormat:formatString arguments:arguments];
-
-	return formattedString;
-}
-
-NSString *_LogToConsoleFormatMessage_v2(LogToConsoleSubsystemType subsystem, u_int8_t type, const char *filename, const char *function, unsigned long line, const char *formatter, ...)
-{
-	NSCParameterAssert(formatter != NULL);
-	NSCParameterAssert(filename != NULL);
-	NSCParameterAssert(function != NULL);
+	dispatch_once(&onceToken, ^{
+		COCOA_EXTENSIONS_DEPRECATED_WARNING
+	});
 
 	va_list arguments;
 	va_start(arguments, formatter);
 
-	NSString *formattedString = _LogToConsoleFormatMessage_v1_arg(subsystem, type, filename, function, line, formatter, arguments);
+	NSString *formattedString = [[NSString alloc] initWithFormat:@(formatter) arguments:arguments];
 
 	va_end(arguments);
 
 	return formattedString;
 }
-
-#pragma mark -
-#pragma mark Deprecated
 
 NSString *_LogToConsoleFormatMessage_v1(u_int8_t type, const char *filename, const char *function, unsigned long line, const char *formatter, ...)
 {
@@ -195,10 +106,16 @@ NSString *_LogToConsoleFormatMessage_v1(u_int8_t type, const char *filename, con
 	NSCParameterAssert(filename != NULL);
 	NSCParameterAssert(function != NULL);
 
+	static dispatch_once_t onceToken;
+
+	dispatch_once(&onceToken, ^{
+		COCOA_EXTENSIONS_DEPRECATED_WARNING
+	});
+
 	va_list arguments;
 	va_start(arguments, formatter);
 
-	NSString *formattedString = _LogToConsoleFormatMessage_v1_arg(LogToConsoleDefaultSubsystem(), type, filename, function, line, formatter, arguments);
+	NSString *formattedString = [[NSString alloc] initWithFormat:@(formatter) arguments:arguments];
 
 	va_end(arguments);
 
@@ -207,50 +124,20 @@ NSString *_LogToConsoleFormatMessage_v1(u_int8_t type, const char *filename, con
 
 void _LogToConsoleNSLogShim(const char *formatter, const char *filename, const char *function, unsigned long line, ...)
 {
-	NSCParameterAssert(formatter != NULL);
-	NSCParameterAssert(filename != NULL);
-	NSCParameterAssert(function != NULL);
+	static dispatch_once_t onceToken;
 
-	static BOOL _deprecationWarningPosted = NO;
-
-	if (_deprecationWarningPosted == NO) {
-		_deprecationWarningPosted = YES;
-
+	dispatch_once(&onceToken, ^{
 		COCOA_EXTENSIONS_DEPRECATED_WARNING
-	}
-
-	va_list arguments;
-	va_start(arguments, line);
-
-	NSString *formattedString = _LogToConsoleFormatMessage_v1_arg(LogToConsoleDefaultSubsystem(), LogToConsoleTypeDefault, filename, function, line, formatter, arguments);
-
-	va_end(arguments);
-
-	NSLog(formattedString);
+	});
 }
 
 void _LogToConsoleNSLogShim_v2(u_int8_t type, const char *filename, const char *function, unsigned long line, const char *formatter, ...)
 {
-	NSCParameterAssert(formatter != NULL);
-	NSCParameterAssert(filename != NULL);
-	NSCParameterAssert(function != NULL);
+	static dispatch_once_t onceToken;
 
-	static BOOL _deprecationWarningPosted = NO;
-
-	if (_deprecationWarningPosted == NO) {
-		_deprecationWarningPosted = YES;
-
+	dispatch_once(&onceToken, ^{
 		COCOA_EXTENSIONS_DEPRECATED_WARNING
-	}
-
-	va_list arguments;
-	va_start(arguments, formatter);
-
-	NSString *formattedString = _LogToConsoleFormatMessage_v1_arg(LogToConsoleDefaultSubsystem(), type, filename, function, line, formatter, arguments);
-
-	va_end(arguments);
-
-	NSLog(formattedString);
+	});
 }
 
 NS_ASSUME_NONNULL_END

Classes/XRLoggingPrivate.h
@@ -32,6 +32,16 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-LogToConsoleSubsystemType _Nullable _CSFrameworkInternalLogSubsystem(void);
+@interface XRLogging : NSObject
+@property (nonatomic, class, strong, nullable) os_log_t defaultSubsystem;
+@property (nonatomic, class, assign) BOOL debugLogging;
+
++ (void)logMessage:(NSString *)message asType:(XRLoggingType)type inSubsystem:(nullable os_log_t)subsystem file:(NSString *)file line:(NSInteger)line column:(NSInteger)column function:(NSString *)function;
++ (void)logStackTraceSymbols:(NSArray<NSString *> *)trace asType:(XRLoggingType)type inSubsystem:(nullable os_log_t)subsystem;
+
+@property (nonatomic, class, strong, nullable) os_log_t frameworkSubsystem;
+@end
+
+os_log_t _Nullable _CSFrameworkInternalLogSubsystem(void);
 
 NS_ASSUME_NONNULL_END

commit 0fcb332b5143c75c8f7d4f7fa45e854c7213ac0d

Fix incorrect variable being logged
Problem introduced by e53723d9a225f72f0da2d051bacc2a02fe60a1c8

Classes/XRLogging.swift
@@ -167,7 +167,7 @@ public class Logging : NSObject
 				}
 			}
 
-			NSLog(formattedMessage)
+			NSLog(messageToLog)
 
 			return
 		}